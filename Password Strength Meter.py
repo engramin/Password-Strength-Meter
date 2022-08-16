10c8c3d1352f16fdf910ab7be209d13bbdfad827
engramin <109396225+engramin@users.noreply.github.com>
2022-08-16 08:01:56 +0500

    Update Password Strength Meter.py

--- Password Strength Meter.py
+++ Password Strength Meter.py
@@ -6,11 +6,12 @@
 # >=15 Characters --- Strong
 
 password=input("Type your Password: ")
+
 if len(password) <=5:
-    print('Your Password is Weak')
+    print('Your Password is Weak,To make your Password Strong use Alpha-Numeric,Lower & Upper Cases,Symbols')
 elif len(password) >=6 and len(password) <=9:
-    print ('Your Password is Fair')
+    print ('Your Password is Fair,To make your Password Strong use Alpha-Numeric,Lower & Upper Cases,Symbols')
 elif len(password) >=10 and len(password) <=14:
-    print('Your Password is Good')
+    print('Your Password is Good but To make your Password Strong use Alpha-Numeric,Lower & Upper Cases,Symbols')
 elif len(password) >=15:
     print('Your Password is Strong')
